1ace7542e1ef17c70b890a85f4e0f6711d499cff
Samira Siddiqua <54817790+samirasidd@users.noreply.github.com>
2021-08-25 17:38:52 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CharacterList from "./components/CharacterList";
+import Episodes from "./components/Episodes";
+
+function App() {
+  return (
+    <div>
+      <h1>Rick And Morty</h1>
+      <CharacterList />
+      <Episodes />
+    </div>
+  );
+}
+
+export default App;
